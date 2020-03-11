a45f9e0d1d9d4684df0ee1358697f26a62e945c3
lucasgabrielx5 <60987409+lucasgabrielx5@users.noreply.github.com>
2020-03-11 19:33:51 -0300

    Add files via upload

---
+++ TextFieldTest/src/textfieldtest/Textfieldtest.java
@@ -0,0 +1,14 @@
+package textfieldtest;
+
+import javax.swing.JFrame;
+
+public class Textfieldtest {
+
+    public static void main(String[] args) {
+        textfieldframe textFieldFrame = new textfieldframe();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350,100);
+        textFieldFrame.setVisible(true);
+    }
+    
+}
